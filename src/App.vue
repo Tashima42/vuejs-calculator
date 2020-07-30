99c5a27e0dab41e6d648f756a90f3e57aede9e65
Pedro Tashima <pedrotashima42@gmail.com>
2020-07-30 19:05:22 -0300

    Added Main display, buttons and logic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculator</h1>
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from "./main/Calculator";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default { 
+    components: {Calculator }
+    };
 </script>
 
 <style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  color: #fff;
+  background: rgb(197, 255, 215);
+  background: linear-gradient(
+    90deg,
+    rgba(197, 255, 215, 1) 0%,
+    rgba(45, 208, 156, 1) 74%,
+    rgba(112, 224, 176, 1) 100%
+  );
 }
 </style>
